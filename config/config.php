18345988345b24737c4277aca44de0be03872513
Astatroth <astatroth@gmail.com>
2017-06-28 00:53:02 +0500

    Added service provider

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'api-url' => 'https://api-maps.yandex.ru/2.0',
+    'static-api-url' => 'https://static-maps.yandex.ru/1.x/',
+    'geocoder-url' => 'https://geocode-maps.yandex.ru/1.x/',
+    'legal-agreement-url' => 'https://legal.yandex.ru/maps_api/',
+    'api-language' => 'ru-RU'
+];
---
+++ src/YandexMapsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Astatroth\LaravelYandexMaps;
+
+use Illuminate\Support\ServiceProvider;
+
+class YandexMapsServiceProvider extends ServiceProvider
+{
+    protected $defer = false;
+
+    public function register()
+    {}
+
+    public function boot()
+    {}
+}
